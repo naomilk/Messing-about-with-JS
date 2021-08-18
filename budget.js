a5ba744193426af1486285b7959071e0644945c2
naomilk <88796940+naomilk@users.noreply.github.com>
2021-08-18 14:05:57 +0100

    Add files via upload

---
+++ budget.js
@@ -0,0 +1,10 @@
+const MinCash = 5000
+const IdealCash = 10000
+let TodaysCash = 6000
+let Expense = 3000
+
+if (TodaysCash < MinCash) {
+    console.log(`Save first until you hit ${MinCash} in the bank.`)
+} else if (TodaysCash > MinCash && TodaysCash < IdealCash && Expense < (TodaysCash-MinCash)) {
+    console.log('You should be good to go!')
+}
